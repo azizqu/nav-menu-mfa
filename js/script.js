8ca62202ae758763abdbcc46e2eda38d29132064
aziz qureshi <azizqu@gmail.com>
2021-10-28 17:09:07 -0400

    created hamburger toggle icon for mobile viewport sizes

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar')
+let navList = document.getElementsByClassName('icon');
+console.log(navList);
+
+let hamburger = navList[0];
+
+hamburger.addEventListener('click',function(){
+        // alert('works!')
+        var x = document.getElementById("myTopNav");
+        if(x.className ==="navbar"){
+            x.className += " responsive";
+        } else{
+            x.className = "navbar";
+        }
+    })
