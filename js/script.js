9279e78b7d4258f7666da60f92b3762ec5a842f8
aziz qureshi <azizqu@gmail.com>
2021-10-31 21:17:39 -0400

    responsive hamburger with close button implemented for mobile screens sizes

--- js/script.js
+++ js/script.js
@@ -4,11 +4,42 @@ console.log(navList);
 let hamburger = navList[0];
 
 hamburger.addEventListener('click',function(){
-        // alert('works!')
-        var x = document.getElementById("myTopNav");
-        if(x.className ==="navbar"){
-            x.className += " responsive";
-        } else{
-            x.className = "navbar";
+    
+        const menu = document.getElementById("myTopNav");
+        const close = document.getElementById("close-btn");
+        if(menu.className ==="navbar"){
+            menu.className += " responsive";
+            close.style.display = "block"
+        } 
+        // else{
+        //     menu.className = "navbar";
+        // }
+        else{
+            close.style.display = "none"
         }
+        
     })
+
+    let closeBtn = document.getElementsByClassName('icon-close');
+    console.log(closeBtn)
+    let closeHam = closeBtn[0];
+    //close ONLY
+    closeHam.addEventListener('click', function(){
+        const x = document.getElementById("myTopNav");
+        const close = document.getElementById("close-btn");
+        if(x.className != "navbar"){
+            if(x.className === "navbar"){
+                x.className += " responsive";
+                
+                
+            }else{
+                x.className = "navbar";
+                close.style.display = "none"
+                
+            }
+            
+        }
+       
+    })
+
+
