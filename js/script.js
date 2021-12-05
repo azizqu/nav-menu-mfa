930fd45d990bc513d01adaee0a2e437a7cf2af30
aziz qureshi <azizqu@gmail.com>
2021-12-05 12:50:15 -0500

    pushed latest blur issue wip

--- js/script.js
+++ js/script.js
@@ -1,53 +1,61 @@
-"use strict"
+'use strict'
 const items = navObj.jsonItems;
-const hamburger = document.getElementById("hamburger");
-const closeBtn = document.getElementById("closebtn");
-const menu = document.getElementById("nav");
-
-const modalClose = document.getElementsByClassName("mclosebtn");
-const listContainer = document.getElementsByClassName("listcontainer")
-const list = document.getElementById("list");
-const navContainerId = document.getElementById("nav-container-id");
+const hamburger = document.getElementById('hamburger');
+const closeBtn = document.getElementById('closebtn');
+const menu = document.getElementById('nav');
+const box = document.getElementById('box');
+const modalClose = document.getElementsByClassName('mclosebtn');
+const listContainer = document.getElementsByClassName('listcontainer')
+//const list = document.getElementById('list');
+const navContainerId = document.getElementById('nav-container-id');
 const timeoutSpeed = 300;
 
 loadNavItems(items);
-initDropdownItems();
 
 
-hamburger.addEventListener("click", function () {
+
+hamburger.addEventListener('click', function () {
     if (!menu.classList.contains('block')) {
         menu.className += ' block';
-        closeBtn.style.display = 'block';
+        box.style.display = 'block';
+        // closeBtn.classList.add()
+        // closeBtn.style.display = 'block';
     } else {
         menu.classList.remove('block');
-        closeBtn.style.display = 'none';
-        list.style.display = 'none';
+        // closeBtn.style.display = 'none';
+      //  list.style.display = 'none';
+        box.style.display = 'none';
         navContainerId.classList.remove('blur');
 
 
     }
 });
-closeBtn.addEventListener("click", function () {
+closeBtn.addEventListener('click', function () {
     menu.className = 'navbar'
-    // closeBtn.classList.remove('hidden');
-    closeBtn.style.display = 'none';
-    list.style.display = 'none';
+    box.style.display = 'none';
+    for (let i =0; i< listContainer.length; i++){
+        listContainer[i].style.display = 'none';
+    }
+
+
+    // listContainer.forEach((l)=>{l.style.display === 'block'?l.style.display = 'none':l.style.display = 'block'});
+
 });
 
 for (let i = 0; i < modalClose.length; i++) {
-    modalClose[i].addEventListener("click", function () {
-        modalClose[i].style.display = "none"
-        list.classList.add('slideIn');
-        list.classList.remove('slideOut');
-        closeBtn.classList.add('hidden');
-        const dropList = document.getElementsByClassName("droplist");
+    modalClose[i].addEventListener('click', function () {
+        modalClose[i].style.display = 'none'
+        listContainer[i].classList.add('slideIn');
+        listContainer[i].classList.remove('slideOut');
+        // closeBtn.classList.add('hidden');
+        const dropList = document.getElementsByClassName('droplist');
         for(let j=0; j < dropList.length; j++){
-            if(dropList[j].classList.contains("droplist")){
-                dropList[j].style.display = "none";
+            if(dropList[j].classList.contains('droplist')){
+                dropList[j].style.display = 'none';
             }
         }
         setTimeout(() => {
-            list.style.display = "none";
+            listContainer[i].style.display = 'none';
             navContainerId.classList.remove('blur');
         }, timeoutSpeed);
     })
@@ -55,72 +63,103 @@ for (let i = 0; i < modalClose.length; i++) {
 
 
 function loadNavItems(items) {
-    const navMenu = document.getElementById("nav");
+    const navMenu = document.getElementById('nav');
 
     for(let i=0; i<items.length; i++){
-        const listTag = document.createElement('li');
-        const aTag = document.createElement('a');
-        aTag.innerHTML = items[i].navTitle
-        aTag.className = items[i].class
-        aTag.href = items[i].href
-        listTag.appendChild(aTag);
-
-        navMenu.appendChild(listTag);
+        const navListTag = document.createElement('li');
+        const aTagNav = document.createElement('a');
+        aTagNav.innerHTML = items[i].navTitle
+        aTagNav.className = items[i].class
+        aTagNav.href = items[i].href
+        navListTag.className = 'navItem '+[i+1];
+        navListTag.appendChild(aTagNav);
 
         if(items[i].dropdown) {
             //create the ul container if dropdowns is true
             //add class etc
             const ulTag = document.createElement('ul');
-            ulTag.className = "droplist";
+            ulTag.className = 'droplist';
             ulTag.classList.add([i]);
-            ulTag.style.display = "none";
-            console.log(items[i].dropdownItems);
-
+            ulTag.style.display = 'none';
 
             const dropdownItems = items[i].dropdownItems;
 
             dropdownItems.forEach((item, index) => {
-                const listTag2 = document.createElement('li');
-                const aTag2 = document.createElement('a');
-                aTag2.innerHTML = item.title;
-                aTag2.href = item.href;
-                listTag2.appendChild(aTag2);
-                ulTag.appendChild(listTag2);
-                list.appendChild(ulTag);
+                const divTagDrop = document.createElement('div');
+                const listTagDrop = document.createElement('li');
+                const aTagDrop = document.createElement('a');
+
+
+                aTagDrop.innerHTML = item.title;
+                aTagDrop.href = item.href;
+                listTagDrop.appendChild(aTagDrop);
+                ulTag.appendChild(listTagDrop);
+                divTagDrop.appendChild(ulTag);
+
+                // list.appendChild(ulTag);
+
+                // navMenu.appendChild(ulTag);
                 //append to newly cvreated ul here
             })
+            const divTag = document.createElement('div');
+            const closeModal = document.createElement('span');
+            const closeIcon = document.createElement('i');
 
+            closeModal.classList.add('modal-close');
+            closeIcon.classList.add('fas');
+            closeIcon.classList.add('fa-arrow-left')
+            closeIcon.classList.add('mclosebtn');
+
+            closeModal.appendChild(closeIcon);
+            divTag.classList.add('listcontainer');
+            divTag.classList.add('dropdown-items');
+           // divTag.id = 'list';
+            navListTag.appendChild(divTag);
+            divTag.appendChild(ulTag);
+            divTag.appendChild(closeModal)
 
         }
+            navMenu.appendChild(navListTag);
 
     }
-
-
+    initDropdownItems();
 }
 
 function initDropdownItems() {
     // bind dropdown list items
-    const dropbtn = document.getElementsByClassName("dropbtn");
-    const dropList = document.getElementsByClassName("droplist");
-    const closeModal = document.getElementById("mclosebtn");
-
+    const dropbtn = document.getElementsByClassName('dropbtn');
+    const dropList = document.getElementsByClassName('droplist');
+    const closeModal = document.getElementsByClassName('mclosebtn');
+    console.log(closeModal)
     for (let i = 0; i < dropbtn.length; i++) {
-        dropbtn[i].addEventListener("click", function () {
-            if (list.classList.contains('slideOut')) {
-                list.classList.remove('slideOut')
-                list.classList.add('slideIn');
-                list.style.display = "none";
-                dropList[i].style.display = "none";
-                closeBtn.style.display = "none";
+        dropbtn[i].addEventListener('click', function (e) {
+            // e.preventDefault();
+            // e.stopPropagation();
+            // e.stopImmediatePropagation()
+            // console.log(e);
+            // console.log(listContainer);
+         //   listContainer.forEach((l) => {
+                if (listContainer[i].classList.contains('slideOut')) {
+                    listContainer[i].classList.remove('slideOut')
+                    listContainer[i].classList.add('slideIn');
+                    listContainer[i].style.display = 'none';
+                    dropList[i].style.display = 'none';
+                    // closeBtn.style.display = 'none';
 
-            } else {
-                list.style.display = "block";
-                dropList[i].style.display = "block";
-                closeModal.style.display = "block";
-                list.classList.add('slideOut');
-                list.classList.remove('slideIn');
-                navContainerId.classList.add('blur');
-            }
+                } else {
+                    listContainer[i].style.display = 'block';
+                    dropList[i].style.display = 'block';
+                    closeModal[i].style.display = 'block';
+                    listContainer[i].classList.add('slideOut');
+                    listContainer[i].classList.remove('slideIn');
+                    // navContainerId.style.filter = 'blur(4px)'
+                    // navContainerId.classList.add('blur');
+
+                    // listContainer[i].style.filter = 'none';
+                    // listContainer[i].classList.add('blur');
+                }
+
+
         })
 
     }
